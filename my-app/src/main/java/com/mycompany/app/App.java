63eb36a594f443aeb98997c00983dbda4038ea4a
apple <289306290@qq.com>
2022-04-29 18:59:29 +0800

    a

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        Random random = new Random();
+        System.out.println(random.nextInt(1)); //永远是0
+        System.out.println(Integer.MAX_VALUE);//Int最大21亿
+        System.out.println(Long.MAX_VALUE);
+
+        int a=1, b=2,c=3;
+        System.out.println(a + b + " aa " + c);
+
+        Range
+
     }
 }
